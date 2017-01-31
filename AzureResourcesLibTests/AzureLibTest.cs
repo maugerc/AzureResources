511eb2779dec0bb541d396f57bbf541b574c61c3
cmauger <mauger.clement@gmail.com>
2017-01-31 23:44:57 +0100

    Add test projects and ncrunch configuration

---
+++ AzureResourcesLibTests/AzureLibTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AzureResourcesLibTests
+{
+    [TestClass]
+    public class AzureLibTest_GetArmMachine
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Utils.Tests/AzureConfigurationTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AzureResources;
+
+namespace Utils.Tests
+{
+    [TestClass]
+    public class AzureConfigurationTest_GetAzureConfig
+    {
+        [TestMethod]
+        public void TestName()
+        {
+            // Arrange
+            // Mock/Stub ReadFile method
+            // Act
+            var result = AzureConfiguration.GetAzureConfig();
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
